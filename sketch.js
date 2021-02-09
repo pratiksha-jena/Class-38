2461689ea519d560e4daf18fb8aedfc49e90ad23
pratiksha-jena <pratikshajena9057@gmail.com>
2021-02-09 17:54:47 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var canvas, backgroundImage;
-
+var car1,car2,car3,car4,cars;
 var gameState = 0;
 var playerCount;
 var allPlayers;
@@ -10,7 +10,7 @@ var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
